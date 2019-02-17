c3164f4d9cd29126103d8cb1a096b3ac96310978
Vincent Occhiogrosso <16623151+vocchiogrosso@users.noreply.github.com>
2019-02-16 21:38:21 -0500

    2.16.2019.D
    + Refractored

--- VersionA/controllers/homePage.js
+++
@@ -1,9 +0,0 @@
-const Post = require('../database/models/Post')
-
-module.exports = async (req, res) => {
-    const posts = await Post.find({})
-    console.log(posts)
-    res.render('index', {
-        posts
-    })
-};
--- VersionB/index.js
+++ VersionB/index.js
@@ -1,18 +1,21 @@
+// NPM
 const expressEdge = require("express-edge");
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
 
+// Controllers
 const createPostController = require('./controllers/createPost')
 const homePageController = require('./controllers/homePage')
 const storePostController = require('./controllers/storePost')
 const getPostController = require('./controllers/getPost')
 
+// Express + Mongo Integration
 const app = new express();
-
 mongoose.connect("mongodb://localhost/node-js-blog");
 
+  // 
 app.use(fileUpload());
 app.use(express.static("public"));
 app.use(expressEdge);
@@ -23,13 +26,15 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 const storePost = require('./middleware/storePost')
 
-app.use('/posts/store', storePost)
+    //Call Middleware
+    app.use('/posts/store', storePost)
+    //Call Controllers
+    app.get("/", homePageController);
+    app.get("/post/:id", getPostController);
+    app.get("/posts/new", createPostController);
+    app.post("/posts/store", storePostController);
 
-app.get("/", homePageController);
-app.get("/post/:id", getPostController);
-app.get("/posts/new", createPostController);
-app.post("/posts/store", storePostController);
-
+// Run On Port
 app.listen(4000, () => {
-  console.log("App listening on port 4000");
+  console.log("App is live at: http://localhost:4000/");
 });
